da6ee9ebc7aeafc1ff2b01585171793d937e9d0c
Tayade <Harshal.Tayade@t-systems.com>
2023-08-31 14:56:22 +0530

    update constant

--- src/main/java/util/CodeUtil.java
+++ src/main/java/util/CodeUtil.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class CodeUtil {
 
-    public static final String CONTACT_SERVICE = "parts_category";
+    public static final String CONTACT_SERVICE = "contact_service";
 }
